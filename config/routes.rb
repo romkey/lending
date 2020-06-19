d3bd02010dc412b30797e3b5247e1d34ee0fa1fe
John Romkey <58883+romkey@users.noreply.github.com>
2020-06-19 12:15:36 -0700

    get users working under /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :loans
-  resources :items
-  resources :categories
   root to: 'home#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
+
+  scope '/admin' do
+    resources :users
+  end
+
+  resources :loans
+  resources :items
+  resources :categories
+
 end
