64b2824c7707680de29df715a2ca4c6cd41fb420
John Romkey <58883+romkey@users.noreply.github.com>
2020-06-18 20:12:25 -0700

    fix recently returned on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @recently_added = Item.order(created_at: :desc).limit(10)
-    @recently_returned = Item.order(returned_at: :desc).limit(10)
+    @recently_returned = Loan.order(returned_at: :desc).limit(10)
   end
 end
