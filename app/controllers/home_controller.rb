f40c1dea293e670521aef24afd6b2676d5ecfb76
John Romkey <58883+romkey@users.noreply.github.com>
2020-06-21 07:50:41 -0700

    work on front page, move status to appropriate form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @recently_added = Item.order(created_at: :desc).limit(10)
-    @recently_returned = Loan.order(returned_at: :desc).limit(10)
+    @recently_returned = Loan.order('returned_at NOT NULL').order(returned_at: :desc).limit(10)
+
+    if can? :manage, Loan
+      @overdue = Loan.where('due_at NOT NULL and due_at < ?', Time.now).order(due_at: :desc)
+    end
   end
 end
