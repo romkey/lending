c65f56526536130c4a831543fb039ae531127824
John Romkey <58883+romkey@users.noreply.github.com>
2020-06-21 07:57:42 -0700

    fixed other NULL test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @recently_added = Item.order(created_at: :desc).limit(10)
-    @recently_returned = Loan.order('returned_at NOT NULL').order(returned_at: :desc).limit(10)
+    @recently_returned = Loan.order('returned_at IS NOT NULL').order(returned_at: :desc).limit(10)
 
     if can? :manage, Loan
       @overdue = Loan.where('due_at IS NOT NULL and due_at < ?', Time.now).order(due_at: :desc)
