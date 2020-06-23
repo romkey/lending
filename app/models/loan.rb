5485aa8f9d3f944bdcc49509366b430db38b8ded
John Romkey <58883+romkey@users.noreply.github.com>
2020-06-22 20:18:55 -0700

    work on loan manageent

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,7 +2,15 @@ class Loan < ApplicationRecord
   belongs_to :user, counter_cache: true
   belongs_to :item, counter_cache: true
 
+  def fulfillable?
+    !complete? && fulfilled_at.nil?
+  end
+
   def overdue?
-    due_at < Time.now
+    due_at < Time.now && !returned_at
+  end
+
+  def complete?
+    !returned_at.nil?
   end
 end
